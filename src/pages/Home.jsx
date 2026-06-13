3d09dcbb3d665aa921b8f41e8e0c2256f8e59ef9
Nityanand Singh <nns.nitya@gmail.com>
2026-06-13 20:04:07 +0530

    react-router-dom: routes added
    for Home, Login & Signup pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>This is home page</h1>
+            <p>Welcome to Home page</p>
+        </div>
+    );
+};
+
+export default Home;
