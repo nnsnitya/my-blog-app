b88ebc5ac824734fb9b3240255b0608539059500
Nityanand Singh <nns.nitya@gmail.com>
2026-08-04 19:34:55 +0530

    Login & Logout managed

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,21 +1,37 @@
-import { useState } from "react";
-import { NavLink as ReactLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { NavLink as ReactLink, useNavigate } from "react-router-dom";
 import {
     Collapse, DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar,
     NavbarBrand, NavbarText, NavbarToggler, NavItem, NavLink, UncontrolledDropdown
 } from "reactstrap";
+import { doLogout, getCurrentUser, isLoggedIn } from "../auth";
 
 const CustomNavbar = (props) => {
 
+    let navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen);
+    const [login, setLogin] = useState(false);
+    const [user, setUser] = useState(undefined);
+
+    useEffect(() => {
+        setLogin(isLoggedIn());
+        setUser(getCurrentUser());
+    }, [login]);
+
+    const logout = () => {
+        doLogout(() => {
+            //logged out
+            setLogin(false);
+            navigate("/");
+        });
+    }
 
     return (
         <div>
             <Navbar {...props} color='dark' dark expand="md" fixed='' className='px-5'>
                 <NavbarBrand tag={ReactLink} to="/">My Blog Logo</NavbarBrand>
-                <NavbarToggler onClick={toggle} />
+                <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="me-auto" navbar>
                         <NavItem>
@@ -42,13 +58,36 @@ const CustomNavbar = (props) => {
                             </DropdownMenu>
                         </UncontrolledDropdown>
                     </Nav>
-                    <Nav>
-                        <NavItem>
-                            <NavLink tag={ReactLink} to="/login">Sign In</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink tag={ReactLink} to="/signup">Sign Up</NavLink>
-                        </NavItem>
+
+                    <Nav navbar>
+                        {
+                            login && (
+                                <>
+                                    <NavItem>
+                                        <NavLink tag={ReactLink} to="/user/profile-info">Profile Info</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink tag={ReactLink} to="/user/dashboard">{user?.name}</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink onClick={logout}>Logout</NavLink>
+                                    </NavItem>
+                                </>
+                            )
+                        }
+                        {
+                            !login && (
+                                <>
+                                    <NavItem>
+                                        <NavLink tag={ReactLink} to="/login">Sign In</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink tag={ReactLink} to="/signup">Sign Up</NavLink>
+                                    </NavItem>
+                                </>
+                            )
+                        }
+
                     </Nav>
                 </Collapse>
             </Navbar>
