a4a60b26fa2f301913f784d736187d649ff6ff46
Nityanand Singh <nns.nitya@gmail.com>
2026-08-31 02:00:02 +0530

    BaseUrl updated, data de-structuring corrected

---
+++ src/services/category-service.js
@@ -0,0 +1,6 @@
+import { myAxios } from "./helper"
+
+export const loadAllCategories = () => {
+    return myAxios
+        .get(`/categories/`).then(resp => { return resp.data.data })
+}
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,11 +2,11 @@ import { myAxios } from "./helper"
 
 export const userSignup = (user) => {
     return myAxios
-        .post('/api/v1/auth/register', user)
+        .post('/auth/register', user)
         .then((resp) => resp.data.data);
 }
 
 export const userSignin = (loginDetail) => {
     return myAxios
-        .post('/api/v1/auth/login', loginDetail).then((resp) => resp.data.data);
+        .post('/auth/login', loginDetail).then((resp) => resp.data.data);
 }
