e48328f99c35d2083045c8258578ab845d6499e8
Nityanand Singh <nns.nitya@gmail.com>
2026-09-01 21:02:47 +0530

    Html(formatting) handled in post

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -6,8 +6,8 @@ const SinglePost = ({ post = { id: -1, title: "This is default post title", cont
         <Card className="shadow-sm mt-3">
             <CardBody>
                 <h1>{post.title}</h1>
-                <CardText>
-                    {post.content.substring(0, 60)}...
+                <CardText dangerouslySetInnerHTML={{ __html: post.content.substring(0, 60) + "..." }}>
+
                 </CardText>
                 <div>
                     <Button>Read More</Button>
