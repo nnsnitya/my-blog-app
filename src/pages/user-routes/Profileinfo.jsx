82eea1db15d91daddb734062ae8e319a63a778b5
Nityanand Singh <nns.nitya@gmail.com>
2026-09-09 22:43:34 +0530

    New/Latest approach to use ContextAPI

--- src/pages/user-routes/Profileinfo.jsx
+++ src/pages/user-routes/Profileinfo.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import Base from "../../components/Base";
+import userContext from "../../context/userContext";
 
 function Profileinfo() {
 
-
+    const user = useContext(userContext);
 
     return (
         <Base>
             <div>
                 <div>profile-info</div>
+                <h1>Welcome, {user.name}</h1>
             </div>
         </Base>
 
